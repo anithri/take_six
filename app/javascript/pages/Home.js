62513239ade6c1cf165bc87ae6101b4d52cd7982
Scott M Parrish <anithri@gmail.com>
2018-07-29 15:08:14 -0600

    tweak

--- app/javascript/pages/Home.js
+++ app/javascript/pages/Home.js
@@ -9,21 +9,7 @@ class HomePage extends React.Component {
       <div id="homePage" className={`pageWrapper ${className}`}>
         {children}
         <main>
-          <h1 class="logo">Hiya Logo</h1>
-          <h1 className="headline">Headline</h1>
-          <h1 className="subheadline">Subheadline</h1>
           <h1>Hiya h1</h1>
-          <h2>Hiya h2</h2>
-          <h3>Hiya h3</h3>
-          <h4>Hiya h4</h4>
-          <h5>Hiya h5</h5>
-          <h6>Hiya h6</h6>
-
-
-
-
-
-
           <p>How are you?</p>
         </main>
       </div>
