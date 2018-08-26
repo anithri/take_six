f7d1422b376f0a42c275f72d788e69985431b32e
Scott M Parrish <anithri@gmail.com>
2018-08-26 14:14:03 -0600

    player spec

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -11,12 +11,20 @@
 #
 
 require 'rails_helper'
+TEST_COUNT = 4
 
 RSpec.describe Player, type: :model do
   describe "Player.random" do
     it 'should return players in a random order' do
       expect(Player.random.to_a).to_not eq Player.random.to_a
     end
+  end
 
+  describe "Player.for_game(count)" do
+    it 'should return <count> Players' do
+      players = Player.for_game(TEST_COUNT)
+      expect(players).to have_attributes length: TEST_COUNT
+      expect(players).to all be_an_instance_of Player
+    end
   end
 end
